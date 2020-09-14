388f536b3f929e3830d5d78440c9b80f7de6c8d9
Ganijon Toshtemurov <tgani96@gmail.com>
2020-09-14 04:50:18 +0300

    files added

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const xlsxFile = require('read-excel-file/node');
+const xlsxFile = require("read-excel-file/node");
+const fs = require("fs");
 
-xlsxFile('./highloadblock.xls').then((rows) => {
+xlsxFile("highloadblock.xlsx")
+  .then((rows) => {
     console.log(rows);
+    rows.forEach((col) => {
+      col.forEach((data) => {
+        console.log(data);
+        console.log(typeof data);
+      });
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-})
+// var XLSX = require("xlsx");
+// var workbook = XLSX.readFile("./highloadblock.xlsx");
+// console.log(workbook);
+// var sheet_name_list = workbook.SheetNames;
+// var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+// console.log(xlData);
